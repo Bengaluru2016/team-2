70409df6c757e4cffd98b2c0985673019d7a9af7
Rozelle <rozelle.jain@gmail.com>
2016-07-09 20:18:57 +0530

    adding survey table update php support
    please validate the survey.html or my code will fail. Validation is of utmost importance. It makes the code accurate.

---
+++ surveyDBUpdate.php
@@ -0,0 +1,29 @@
+<?php
+
+$con = mysql_connect("localhost","root","password");
+if (!$con)
+{
+	echo "fail";
+	//header('Location: http://10.242.6.72/samridhi/fail.html');
+}
+	
+else
+{
+	mysql_select_db("samridhi", $con);
+	$sql="INSERT INTO 
+	survey(first_name, last_name, dob, gender, father, father_occupation, mother, mother_occupation, language, income, adhar_card_number, location, living_period, school_before, contact_number, alt_contact_number) 
+	VALUES ('$_POST[Firstname]', '$_POST[Lastname]', '$_POST[DOB]', '$_POST[gender]', '$_POST[Fathername]', '$_POST[Foccupation]', '$_POST[Mothername]', '$_POST[Moccupation]', '$_POST[Language]', '$_POST[Income]', '$_POST[Aadhar]', '$_POST[Location]', '$_POST[Living]', '$_POST[sch]', '$_POST[Contact]', '$_POST[AltContact]') ";
+	if (!mysql_query($sql,$con))
+	{
+		echo "insert error";
+		//header('Location: http://10.242.6.72/samridhi/fail.html');
+	}
+	else
+	{
+		header('Location: http://10.242.6.72/samridhi/survy.html');
+		exit();
+    }
+	mysql_close($con);
+}
+
+?>
